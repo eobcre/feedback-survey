992391ae11b05c6b4191286f2df89f78a3768717
eobcre <eobcre@gmail.com>
2023-08-30 20:38:09 -0400

    placed components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import Answer from './components/Answer';
+import NextButton from './components/NextButton';
+import PageDisplay from './components/PageDisplay';
+import PreviousButton from './components/PreviousButton';
+import Question from './components/Question';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <PageDisplay />
+      <Question />
+      <Answer />
+      <PreviousButton />
+      <NextButton />
+    </div>
+  );
 };
 
 export default App;
