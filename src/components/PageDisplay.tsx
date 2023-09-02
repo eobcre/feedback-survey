4bdc228c12caded91a2840d92799b987859145e7
eobcre <eobcre@gmail.com>
2023-09-01 21:44:58 -0400

    page styled

--- src/components/PageDisplay.tsx
+++ src/components/PageDisplay.tsx
@@ -1,5 +1,9 @@
 const PageDisplay: React.FC = () => {
-  return <div>PageDisplay</div>;
+  return (
+    <div className='flex justify-center items-center mt-10 mb-5'>
+      <p>1/3</p>
+    </div>
+  );
 };
 
 export default PageDisplay;
