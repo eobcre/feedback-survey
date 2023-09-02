18ce20e8d6c744fac662169af59611e43c2f9528
eobcre <eobcre@gmail.com>
2023-09-01 21:48:22 -0400

    created question data

---
+++ src/data/questions.tsx
@@ -0,0 +1,11 @@
+export const questions = [
+  {
+    question: 'You need to know how to code to be a UI/UX designer',
+  },
+  {
+    question: 'Everyday is a progress',
+  },
+  {
+    question: 'You are specialized in React.js',
+  },
+];
