0e21f6b3222dc8a2c0b4109e4fc44f4a10fe1c0a
eobcre <eobcre@gmail.com>
2023-09-01 21:42:42 -0400

    updated data import, next and previous button logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
+import { useState } from 'react';
+
 import Answer from './components/Answer';
 import Button from './components/Button';
 import PageDisplay from './components/PageDisplay';
 import Question from './components/Question';
 
+import { questions } from './data/questions';
+
 const App = () => {
+  const [questionIndex, setQuestionIndex] = useState(0);
+
+  // buttons
   const handleClickPrevious = () => {
-    console.log('previous');
+    questionIndex > 0 ? setQuestionIndex(questionIndex - 1) : '';
   };
 
   const handleClickNext = () => {
-    console.log('next');
+    questionIndex < questions.length - 1
+      ? setQuestionIndex(questionIndex + 1)
+      : '';
   };
 
   return (
-    <div>
-      <PageDisplay />
-      <Question />
-      <Answer />
-      <Button onClick={handleClickNext} label='Previous' />
-      <Button onClick={handleClickPrevious} label='Next' />
+    <div className='flex justify-center items-center bg-[#F4FAFF] h-screen'>
+      <div className='bg-white rounded-xl px-16 w-[600px] h-[330px]'>
+        <PageDisplay />
+        <Question questionIndex={questionIndex} />
+        <Answer />
+        <div className='flex justify-between mt-16'>
+          <Button
+            onClick={handleClickPrevious}
+            label='Previous'
+            disabled={questionIndex === 0}
+            className='border border-black rounded py-2 w-[120px]'
+          />
+          <Button
+            onClick={handleClickNext}
+            label='Next'
+            disabled={questionIndex === questions.length - 1}
+            className='bg-[#567FEA] text-white rounded py-2 w-[120px]'
+          />
+        </div>
+      </div>
     </div>
   );
 };
