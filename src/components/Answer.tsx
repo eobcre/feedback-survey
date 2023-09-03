3a0fb01996914f2fdb58bb1f6e6dba964d54a161
eobcre <eobcre@gmail.com>
2023-09-03 13:59:44 -0400

    label and input map loop logic

--- src/components/Answer.tsx
+++ src/components/Answer.tsx
@@ -1,5 +1,35 @@
 const Answer: React.FC = () => {
-  return <div>Answer</div>;
+  const inputAnswers = [
+    'Totally disagree',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    'Totally agree',
+  ];
+
+  return (
+    <div className='flex justify-center'>
+      {inputAnswers.map((answer, index) => (
+        <div key={index} className='relative'>
+          <label
+            htmlFor={`linked-${index}`}
+            className='absolute top-[2rem] left-[-1.1rem] text-xs w-[100px]'
+          >
+            {answer}
+          </label>
+          <input
+            type='checkbox'
+            id={`linked-${index}`}
+            className='h-3 w-3 rounded-full border-2 border-[#B1CDF1] appearance-none cursor-pointer checked:bg-black checked:border-black'
+          />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Answer;
