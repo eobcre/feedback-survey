bf7e0c676427e42786b7104d7c0b315a9b2f5577
eobcre <eobcre@gmail.com>
2023-09-05 13:14:12 -0400

    initial state updated to 7, updated style

--- src/components/Answer.tsx
+++ src/components/Answer.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 
 const Answer: React.FC = () => {
-  const [clicked, setClicked] = useState<number | null>(null);
+  const [clicked, setClicked] = useState<number | null>(7);
 
   const inputAnswers = [
     'Totally disagree',
+    '',
     '',
     '',
     '',
@@ -22,21 +23,21 @@ const Answer: React.FC = () => {
   return (
     <div className='flex justify-center'>
       {inputAnswers.map((answer, index) => (
-        <div key={index} className='relative'>
-          <label
-            htmlFor={`linked-${index}`}
-            className='absolute top-[2rem] left-[-1.1rem] text-xs w-[100px]'
-          >
-            {answer}
-          </label>
+        <div className='relative'>
           <input
             type='checkbox'
             id={`linked-${index}`}
             onClick={() => handleClick(index)}
-            className={`border-2 border-[#B1CDF1] rounded-full appearance-none cursor-pointer w-3 h-3 ${
+            className={`relative inline-block border-2 border-[#B1CDF1] rounded-full appearance-none cursor-pointer mx-4 w-3 h-3 before:content-[''] before:absolute before:top-[3px] before:left-[10px] before:w-[2.1rem] before:h-[0.1rem] before:bg-[#B1CDF1] ${
               clicked === index ? 'bg-black border-black' : ''
             }`}
           />
+          <label
+            htmlFor={`linked-${index}`}
+            className='absolute top-[2rem] left-[-1rem] text-xs w-[100px]'
+          >
+            {answer}
+          </label>
         </div>
       ))}
     </div>
