cd8b0d76bc6a49c966b18edc1e55e5550dd09ef6
eobcre <eobcre@gmail.com>
2023-09-06 12:24:56 -0400

    created props, updated style, display page

--- src/components/PageDisplay.tsx
+++ src/components/PageDisplay.tsx
@@ -1,7 +1,11 @@
-const PageDisplay: React.FC = () => {
+type PageProps = {
+  currentPage: number;
+};
+
+const PageDisplay: React.FC<PageProps> = ({ currentPage }) => {
   return (
-    <div className='flex justify-center items-center mt-10 mb-5'>
-      <p className='text-gray-400 font-Arial'>1/3</p>
+    <div className='flex justify-center items-center mt-10'>
+      <p className='text-gray-400 font-Arial'>{currentPage}/3</p>
     </div>
   );
 };
