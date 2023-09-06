4a90fb4bba312022eb45bd0d74d99a7e748652ca
eobcre <eobcre@gmail.com>
2023-09-06 12:23:52 -0400

    created page logic, props

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,32 @@ import { useState } from 'react';
 import Answer from './components/Answer';
 import Button from './components/Button';
 import PageDisplay from './components/PageDisplay';
+import ProgressBar from './components/ProgressBar';
 import Question from './components/Question';
 
 import { questions } from './data/questions';
 
 const App = () => {
   const [questionIndex, setQuestionIndex] = useState(0);
+  const [currentPage, setCurrentPage] = useState<number>(1);
 
   // buttons
   const handleClickPrevious = () => {
     questionIndex > 0 ? setQuestionIndex(questionIndex - 1) : '';
+    currentPage > 1 ? setCurrentPage((prevState) => prevState - 1) : '';
   };
 
   const handleClickNext = () => {
     questionIndex < questions.length - 1
       ? setQuestionIndex(questionIndex + 1)
       : '';
+    currentPage < 3 ? setCurrentPage((prevState) => prevState + 1) : '';
   };
 
   return (
     <div className='flex justify-center items-center bg-[#F4FAFF] h-screen'>
       <div className='bg-white rounded-xl px-16 w-[600px] h-[330px]'>
-        <PageDisplay />
+        <PageDisplay currentPage={currentPage} />
         <Question questionIndex={questionIndex} />
         <Answer />
         <div className='flex justify-between mt-16'>
