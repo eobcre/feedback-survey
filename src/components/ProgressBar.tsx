dd26e05b876c635eceec9789e94e9113553951d4
eobcre <eobcre@gmail.com>
2023-09-06 21:58:05 -0400

    progress bar logic and styled

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,5 +1,20 @@
-const ProgressBar = () => {
-  return <div>ProgressBar</div>;
+type ProgressProps = {
+  progressCal: () => number;
+};
+
+const ProgressBar: React.FC<ProgressProps> = ({ progressCal }) => {
+  const progressWidth = progressCal();
+
+  return (
+    <div className='flex justify-center items-center mb-5'>
+      <span className='bg-[#E7F3FF] rounded-full w-[120px] h-[6px]'>
+        <span
+          style={{ width: `${progressWidth}px` }}
+          className={`absolute z-10 bg-[#303D62] rounded-full h-[6px]`}
+        ></span>
+      </span>
+    </div>
+  );
 };
 
 export default ProgressBar;
