87a3a8cb4a988391f42fdd6dbb0b949e87760ca3
eobcre <eobcre@gmail.com>
2023-09-08 22:11:53 -0400

    updated input and label style, simplified the conditional statements

--- src/components/Answer.tsx
+++ src/components/Answer.tsx
@@ -23,24 +23,22 @@ const Answer: React.FC = () => {
   return (
     <div className='flex justify-center'>
       {inputAnswers.map((answer, index) => (
-        <div className='relative'>
+        <div className='relative' key={index}>
           <input
             type='radio'
             id={`linked-${index}`}
             onClick={() => handleClick(index)}
-            className={`relative inline-block border-2 border-[#B1CDF1] rounded-full appearance-none cursor-pointer mx-4 w-3 h-3 ${
-              index < 9
-                ? `after:content-[''] after:absolute after:top-[3px] after:left-[10px] after:w-[2.1rem] after:h-[0.1rem] after:bg-[#B1CDF1]`
-                : ''
+            className={`relative top-[10px] left-[10px] border-2 border-[#B1CDF1] rounded-full appearance-none cursor-pointer mx-4 w-3 h-3 ${
+              index < 9 &&
+              `after:content-[''] after:absolute after:top-[3px] after:left-[10px] after:w-[2.1rem] after:h-[0.1rem] after:bg-[#B1CDF1]`
             } ${
-              clicked === index
-                ? 'bg-black border-[#F4F9FF] shadow scale-150 after:w-[1.28rem] after:h-[0.07rem]'
-                : ''
+              clicked === index &&
+              'test absolute bg-black border-[#F4F9FF] shadow'
             }`}
           />
           <label
             htmlFor={`linked-${index}`}
-            className='absolute top-[2rem] left-[-1rem] text-xs w-[100px]'
+            className='absolute top-[2.2rem] left-[-1rem] text-xs w-[100px]'
           >
             {answer}
           </label>
